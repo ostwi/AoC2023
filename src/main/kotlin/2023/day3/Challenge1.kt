c6e1905ab2fd95079ec6a853878785a3ec8055bc
Jakub Ostwald <jakub.ostwald@flipapp.de>
2023-12-02 15:00:48 +0100

    extend template for day 3

---
+++ src/main/kotlin/2023/day3/Challenge1.kt
@@ -0,0 +1,18 @@
+package `2023`.day3
+
+import java.io.File
+
+class Challenge1 {
+    private val testData: List<String> = File("src/main/resources/2023/Day2/2-test.txt").readLines()
+    private val data: List<String> = File("src/main/resources/2023/Day2/2.txt").readLines()
+
+    fun run() {
+        println(getResult())
+    }
+
+    private fun getResult(): Int {
+        return 0
+    }
+
+}
+
