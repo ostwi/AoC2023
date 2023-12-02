590049be3e08354992de917c7de943252ff22617
Jakub Ostwald <jakub.ostwald@flipapp.de>
2023-12-02 11:20:37 +0100

    refactor

--- src/main/kotlin/2023/day2/Challenge1.kt
+++ src/main/kotlin/2023/day2/Challenge1.kt
@@ -1,7 +1,6 @@
 package `2023`.day2
 
 import java.io.File
-import java.lang.RuntimeException
 
 class Challenge1 {
     val testData: List<String> = File("src/main/resources/2023/Day2/2-1-test.txt").readLines()
@@ -17,45 +16,4 @@ class Challenge1 {
         return possibleGames.map { it.gameId }.sum()
     }
 
-    private fun parseGame(game: String): Game {
-        val roundId = game.substringAfter("Game ").substringBefore(":").toInt()
-        val rounds = game.substringAfter("Game $roundId: ")
-            .split(";")
-            .map { it.trim() }
-            .map { revelation ->
-                revelation.split(",")
-                    .map { it.trim() }
-                    .map { cubeSet ->
-                        parseCubeSet(cubeSet)
-                    }
-            }
-            .map { sets ->
-                Round(
-                    red = sets.filter { it.color == CubeColor.RED }.sumOf { it.amount },
-                    green = sets.filter { it.color == CubeColor.GREEN }.sumOf { it.amount },
-                    blue = sets.filter { it.color == CubeColor.BLUE }.sumOf { it.amount }
-                )
-            }
-
-        return Game(
-            gameId = roundId,
-            rounds = rounds
-        )
-    }
-
-    private fun parseCubeSet(input: String): CubeSet {
-        val data = input.split(" ")
-        val color = parseColor(data.last())
-        val amount = data.first().toInt()
-        return CubeSet(color, amount)
-    }
-
-    private fun parseColor(color: String): CubeColor {
-        return when(color) {
-            "green" -> CubeColor.GREEN
-            "red" -> CubeColor.RED
-            "blue" -> CubeColor.BLUE
-            else -> throw RuntimeException(color)
-        }
-    }
 }
--- src/main/kotlin/2023/day2/Game.kt
+++ src/main/kotlin/2023/day2/Game.kt
@@ -1,5 +1,7 @@
 package `2023`.day2
 
+import java.lang.RuntimeException
+
 data class Game(
     val gameId: Int,
     val rounds: List<Round>
@@ -28,4 +30,46 @@ enum class CubeColor {
     GREEN,
     BLUE,
     RED,
+}
+
+fun parseGame(game: String): Game {
+    val roundId = game.substringAfter("Game ").substringBefore(":").toInt()
+    val rounds = game.substringAfter("Game $roundId: ")
+        .split(";")
+        .map { it.trim() }
+        .map { revelation ->
+            revelation.split(",")
+                .map { it.trim() }
+                .map { cubeSet ->
+                    parseCubeSet(cubeSet)
+                }
+        }
+        .map { sets ->
+            Round(
+                red = sets.filter { it.color == CubeColor.RED }.sumOf { it.amount },
+                green = sets.filter { it.color == CubeColor.GREEN }.sumOf { it.amount },
+                blue = sets.filter { it.color == CubeColor.BLUE }.sumOf { it.amount }
+            )
+        }
+
+    return Game(
+        gameId = roundId,
+        rounds = rounds
+    )
+}
+
+private fun parseCubeSet(input: String): CubeSet {
+    val data = input.split(" ")
+    val color = parseColor(data.last())
+    val amount = data.first().toInt()
+    return CubeSet(color, amount)
+}
+
+private fun parseColor(color: String): CubeColor {
+    return when(color) {
+        "green" -> CubeColor.GREEN
+        "red" -> CubeColor.RED
+        "blue" -> CubeColor.BLUE
+        else -> throw RuntimeException(color)
+    }
 }
