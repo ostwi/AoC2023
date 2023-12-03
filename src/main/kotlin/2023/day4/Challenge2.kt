d8f276d2bfa0d26e6d285f1634f4067cdb6134f3
Jakub Ostwald <jakub.ostwald@flipapp.de>
2023-12-03 15:32:55 +0100

    add template for day 4

---
+++ src/main/kotlin/2023/day4/Challenge2.kt
@@ -0,0 +1,17 @@
+package `2023`.day4
+
+import java.io.File
+
+class Challenge2 {
+//    private val data: List<String> = File("src/main/resources/2023/Day4/4-test.txt").readLines()
+    private val data: List<String> = File("src/main/resources/2023/Day4/4.txt").readLines()
+
+    fun run() {
+        println(getResult())
+    }
+
+    private fun getResult(): Int {
+        return 0
+    }
+}
+
