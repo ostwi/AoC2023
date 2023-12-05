0325e86b9be7437c3b4700ffbcaae65e2a33017d
Jakub Ostwald <jakub.ostwald@flipapp.de>
2023-12-05 10:55:31 +0100

    add template for Day 6

---
+++ src/main/kotlin/2023/day6/Challenge1.kt
@@ -0,0 +1,17 @@
+package `2023`.day6
+
+import java.io.File
+
+class Challenge1 {
+    private val data: List<String> = File("src/main/resources/2023/Day6/test.txt").readLines()
+//    private val data: List<String> = File("src/main/resources/2023/Day6/data.txt").readLines()
+
+    fun run() {
+        println(getResult())
+    }
+
+    private fun getResult(): Long {
+        return 0
+    }
+}
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
-import `2023`.day5.Challenge1
-import `2023`.day5.Challenge2
+import `2023`.day6.Challenge1
+import `2023`.day6.Challenge2
 
 fun main(args: Array<String>) {
     println("Challenge 1:")
-//    Challenge1().run()
+    Challenge1().run()
     println("========================================")
     println("Challenge 2:")
     Challenge2().run()
