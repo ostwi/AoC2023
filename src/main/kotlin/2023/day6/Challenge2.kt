eba25f9fc7a90dc499478c7561cd9f6c650204a9
Jakub Ostwald <jakub.ostwald@flipapp.de>
2023-12-06 07:03:07 +0100

    Day 6 Challenge 2

--- src/main/kotlin/2023/day6/Challenge2.kt
+++ src/main/kotlin/2023/day6/Challenge2.kt
@@ -3,15 +3,17 @@ package `2023`.day6
 import java.io.File
 
 class Challenge2 {
-    private val data: List<String> = File("src/main/resources/2023/Day6/test.txt").readLines()
-//    private val data: List<String> = File("src/main/resources/2023/Day6/data.txt").readLines()
+//    private val data: List<String> = File("src/main/resources/2023/Day6/test.txt").readLines()
+    private val data: List<String> = File("src/main/resources/2023/Day6/data.txt").readLines()
 
     fun run() {
         println(getResult())
     }
 
     private fun getResult(): Long {
-        return 0
+        val race = parseBigRace(data)
+        println(race)
+        return getFinalResult(race)
     }
 }
 
--- src/main/kotlin/2023/day6/Helper.kt
+++ src/main/kotlin/2023/day6/Helper.kt
@@ -5,6 +5,11 @@ data class Race(
     val distance: Int,
 )
 
+data class BigRace(
+    val time: Long,
+    val distance: Long,
+)
+
 fun parseStringToIntList(input: String): List<Int> {
     return input
         .substringAfter(": ")
@@ -23,15 +28,39 @@ fun getRaces(size: Int, times: List<Int>, distances: List<Int>): List<Race> {
     return races
 }
 
-fun parseBigRace(input: String): Long =
-    input
+fun parseBigRace(input: List<String>): BigRace {
+    val givenTime = input[0]
         .substringAfter(": ")
         .trim()
         .split(" ")
         .map { it.trim() }
         .filter { it.isNotEmpty() }
-        .joinToString { it }
+        .reduce(String::plus)
         .toLong()
+
+    val givenDistance = input[1]
+        .substringAfter(": ")
+        .trim()
+        .split(" ")
+        .map { it.trim() }
+        .filter { it.isNotEmpty() }
+        .reduce(String::plus)
+        .toLong()
+
+    return BigRace(givenTime, givenDistance)
+}
+
+fun getFinalResult(race: BigRace): Long {
+    var recordSolutions = 0L
+    for (i in LongRange(0, race.time)) {
+        val distance = calculateDistance(race.time, i)
+        val solution = BigSolution(i, distance)
+        if (solution.distance > race.distance) {
+            recordSolutions++
+        }
+    }
+    return recordSolutions
+}
 
 fun getRecordSolutions(race: Race): Int {
     var recordSolutions = 0
@@ -49,8 +78,17 @@ fun calculateDistance(givenTime: Int, holdingTime: Int): Int {
     return (givenTime - holdingTime) * holdingTime
 }
 
+fun calculateDistance(givenTime: Long, holdingTime: Long): Long {
+    return (givenTime - holdingTime) * holdingTime
+}
+
 data class Solution(
     val holdingTime: Int,
     val distance: Int,
+)
+
+data class BigSolution(
+    val holdingTime: Long,
+    val distance: Long,
 )
 
