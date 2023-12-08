259352667d08ee5bd857cf9226e1de6cbb06915d
Jakub Ostwald <jakub.ostwald@flipapp.de>
2023-12-08 08:28:41 +0100

    Day 8 Challenge 2

--- src/main/kotlin/2023/day8/Challenge2.kt
+++ src/main/kotlin/2023/day8/Challenge2.kt
@@ -3,8 +3,8 @@ package `2023`.day8
 import java.io.File
 
 class Challenge2 {
-        private val data: List<String> = File("src/main/resources/2023/Day7/test.txt").readLines()
-//    private val data: List<String> = File("src/main/resources/2023/Day7/data.txt").readLines()
+//        private val data: List<String> = File("src/main/resources/2023/Day8/test.txt").readLines()
+    private val data: List<String> = File("src/main/resources/2023/Day8/data.txt").readLines()
 
     fun run() {
         println(getResult())
@@ -14,35 +14,24 @@ class Challenge2 {
         val instructions = data[0].chars().toArray().map { it.toChar() }
         val positions = data.subList(2, data.size).map { it.getNode() }
         val positionsMap = positions.associateBy { it.position }
-        var lastPosition = "AAA"
-        var result = 0L
-        var finished = false
-        var counter = 0
+        var lastPositions = positions.map { it.position }.filter { it.endsWith("A") }
 
-        while (!finished) {
-            result++
-            val position = positionsMap[lastPosition]!!
-            when (instructions[counter]) {
-                'L' -> {
-                    lastPosition = position.left
-                    if (position.left == "ZZZ") {
-                        finished = true
-                    }
-                }
-                'R' -> {
-                    lastPosition = position.right
-                    if (position.right == "ZZZ") {
-                        finished = true
-                    }
-                }
-            }
-            counter++
-            if (counter >= instructions.size) {
-                counter = 0
-            }
-        }
-        return result
+        val steps = lastPositions
+            .map {
+                getLast(instructions, positionsMap, it)
+            }.map { it.toLong() }
+
+        return steps.reduce { total, next -> lcm(total, next) }
     }
+
+    fun gcd(a: Long, b: Long): Long {
+        return if (b == 0L) a else gcd(b, a % b)
+    }
+
+    fun lcm(a: Long, b: Long): Long {
+        return a / gcd(a, b) * b
+    }
+
 
     fun String.getNode(): Node {
         return Node(
@@ -57,5 +46,35 @@ class Challenge2 {
         val left: String,
         val right: String
     )
+
+    fun getLast(instructions: List<Char>, positionsMap: Map<String, Node>, it: String): Int {
+        var counter = 0
+        var done = false
+        var res = 0
+        var last = positionsMap[it]!!.position
+        while (!done) {
+            res++
+            val position = positionsMap[last]!!
+            when (instructions[counter]) {
+                'L' -> {
+                    last = position.left
+                    if (position.left.endsWith("Z")) {
+                        done = true
+                    }
+                }
+                'R' -> {
+                    last = position.right
+                    if (position.right.endsWith("Z")) {
+                        done = true
+                    }
+                }
+            }
+            counter++
+            if (counter >= instructions.size) {
+                counter = 0
+            }
+        }
+        return res
+    }
 }
 
