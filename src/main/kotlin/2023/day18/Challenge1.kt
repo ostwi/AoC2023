789879df63d3426bc5f6315a6e341aa07cd452ba
webspaceadam <adam.silber-gniady@getflip.com>
2023-12-18 07:54:25 +0100

    added logging for debugging

--- src/main/kotlin/2023/day18/Challenge1.kt
+++ src/main/kotlin/2023/day18/Challenge1.kt
@@ -4,12 +4,13 @@ import kotlin.math.*
 import java.io.File
 
 class Challenge1 {
-    private val data: List<String> = File("src/main/resources/2023/Day18/adam-1.txt").readLines()
+    private val data: List<String> = File("src/main/resources/2023/Day18/test.txt").readLines()
 
     fun run() {
         val lagoonArea = getResult(data)
         println("Cubic meters of lava the lagoon can hold: $lagoonArea")
     }
+
 
     private fun getResult(input: List<String>): Int {
         val lagoonEdges = HashSet<Pair<Int, Int>>()
@@ -20,7 +21,7 @@ class Challenge1 {
         var minY = 0
         var maxY = 0
 
-        for (cmd in input) {
+        input.forEach { cmd ->
             val parts = cmd.split(" ")
             val direction = parts[0][0]
             val steps = parts[1].split("(")[0].toInt()
@@ -38,18 +39,36 @@ class Challenge1 {
                 minY = minOf(minY, y)
                 maxY = maxOf(maxY, y)
             }
+
+            printLagoon(minX, minY, maxX, maxY, lagoonEdges, emptySet())
         }
 
-        var interiorCount = 0
+        val interior = mutableSetOf<Pair<Int, Int>>()
         for (i in minX..maxX) {
             for (j in minY..maxY) {
                 if (Pair(i, j) !in lagoonEdges && isEnclosed(i, j, lagoonEdges, minX, maxX, minY, maxY)) {
-                    interiorCount++
+                    interior.add(Pair(i, j))
                 }
             }
         }
 
-        return interiorCount + lagoonEdges.size
+        printLagoon(minX, minY, maxX, maxY, lagoonEdges, interior)
+        return interior.size + lagoonEdges.size
+    }
+
+    private fun printLagoon(minX: Int, minY: Int, maxX: Int, maxY: Int, edges: Set<Pair<Int, Int>>, interior: Set<Pair<Int, Int>>) {
+        println("Current state of the lagoon:")
+        for (j in minY..maxY) {
+            for (i in minX..maxX) {
+                when {
+                    Pair(i, j) in edges -> print("#")
+                    Pair(i, j) in interior -> print("#")
+                    else -> print(".")
+                }
+            }
+            println()
+        }
+        println()
     }
 
     private fun isEnclosed(x: Int, y: Int, edges: Set<Pair<Int, Int>>, minX: Int, maxX: Int, minY: Int, maxY: Int): Boolean {
